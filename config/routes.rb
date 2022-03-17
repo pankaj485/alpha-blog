1a937b4fe4c551fa59149136ad6c81e264488f63
pankcy <pankaj@cloudyuga.guru>
2022-03-17 13:16:12 +0530

    add model and route to article show method to list particular entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/home'
+  get "pages/about"
+  get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root "pages#home"
-
   get "about", to: "pages#about"
+  resources :articles, only: [:show]
 end
