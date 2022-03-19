859fd95da1a8e6ed506d6a5d161d5f63cec627b9
pankcy <pankaj@cloudyuga.guru>
2022-03-19 09:47:50 +0530

    resolve issue of not able to delete while edit message is shown.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
+// line 5 caused error on if i'm able to delete then i'm not able to show error
+// import "@hotwired/turbo-rails";
 import "controllers";
