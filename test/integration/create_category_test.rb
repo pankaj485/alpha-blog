80491fcaa16160695f74e2ee60e186d52a3135f1
pankaj485 <pankajsamar2@gmail.com>
2022-04-14 13:30:28 +0530

    add integration test for rejected form submisstion for categoryJ

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -19,4 +19,28 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     # make sure to match the content is present after redirection
     assert_match "Sports", response.body
   end
+
+  require "test_helper"
+
+  test "get new category form and reject invalid category submission" do
+    # get new categories path
+    get "/categories/new"
+
+    # verify the path is giving success response
+    assert_response :success
+
+    # for invalid submission, differene is 0 sicne no new category will be created
+    assert_no_difference("Category.count") do
+      # creating a post request to categories_url with the params provided
+      # providing no value so that it counts as invalid case.
+      post categories_url, params: { category: { name: " " } }
+    end
+
+    # verify error by checking the content of the error messages.
+    assert_match "errors", response.body
+
+    # verify error by checking the classnames of the error elements.
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
+  end
 end
