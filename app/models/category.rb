29cfd56488a9073c31a910c7296b8bcdb27e1e79
pankaj485 <pankajsamar2@gmail.com>
2022-04-15 10:45:00 +0530

    add many-to-many associations, add association form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+  # associating categoreis and articles table using has_many and has_many: :through
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
