cb08e4079f9d63597ac90e42fd82fd22490acc34
pankaj485 <pankajsamar2@gmail.com>
2022-04-15 11:51:56 +0530

    add edit categoreis functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_admin, except: [:index, :show]
 
   def new
@@ -12,6 +12,18 @@ class CategoriesController < ApplicationController
 
   def show
     @articles = @category.articles.paginate(page: params[:page], per_page: 3)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render "edit"
+    end
   end
 
   def create
